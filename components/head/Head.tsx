528ecca50e922b0bfe2fbad0f1ad3634b30a1420
aaandrades <aaandrades@outlook.com>
2022-11-22 21:46:19 -0500

    :alembic: Added metadata

--- components/head/Head.tsx
+++ components/head/Head.tsx
@@ -8,8 +8,16 @@ const HeadComponent = () => {
         name="description"
         content="Andres Alexander Andrade Sanchez, Fullstack software developer based in Mississauga, Ontario - Canada"
       />
-      {/* <link rel="icon" href="/code.svg" /> */}
-      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:url" content="https://andres-andrade.dev/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Andres Andrade" />
+      <meta name="twitter:card" content="summary" />
+      <meta
+        property="og:description"
+        content="Andres Alexander Andrade Sanchez, Fullstack software developer based in Mississauga, Ontario - Canada"
+      />
+      <meta property="og:image" content="/preview.jpg" />
+      <link rel="icon" href="/code.png" />
     </Head>
   );
 };
