cd15feb310ea4317bfbf1466f298f6140c18d7a5
aaandrades <aaandrades@outlook.com>
2022-11-22 21:22:01 -0500

    :alembic: Adding meta information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import styles from "../styles/Home.module.css";
 import HeadComponent from "../components/head/Head";
 import MainComponent from "../components/main/Main";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <HeadComponent />
-      <MainComponent />
+      <Head>
+        <title>Andres Andrade</title>
+        <meta
+          name="description"
+          content="Andres Alexander Andrade Sanchez, Fullstack software developer based in Mississauga, Ontario - Canada"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <HeadComponent /> */}
+      <main>
+        <h1>Page Works</h1>
+      </main>
+      {/* <MainComponent /> */}
     </div>
   );
 }
