3cbf76199142642a27beb216d0ba703fd62a6c6c
aaandrades <aaandrades@outlook.com>
2022-11-26 11:50:36 -0500

    :alembic: Added swiper

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,7 +1,23 @@
 import Layout from "../layout/Layout";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const About = () => {
-  return <Layout id="about">About</Layout>;
+  return (
+    <Layout id="about">
+      <h2>Contenido</h2>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </Layout>
+  );
 };
 
 export default About;
